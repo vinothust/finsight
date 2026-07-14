792ddfce03d03d6bb5efc43d2f148fc0010c1a21
Vinoth Sasikumar <vinoth.sasikumar@ust.com>
2026-07-14 19:27:38 +0530

    chore: register new models in app.models

--- apps/api/app/models/__init__.py
+++ apps/api/app/models/__init__.py
@@ -1,9 +1,25 @@
 from app.models.account import Account
+from app.models.associations import AccountDirector, ClusterHead, ProjectManager
 from app.models.cluster import Cluster
 from app.models.financial_record import FinancialRecord
 from app.models.llm_settings import LLMSettings
 from app.models.project import Project
+from app.models.refresh_token import RefreshToken
 from app.models.upload import Upload
+from app.models.user import User
 from app.models.utilization_record import UtilizationRecord
 
-__all__ = ["Account", "Cluster", "Project", "FinancialRecord", "UtilizationRecord", "Upload", "LLMSettings"]
+__all__ = [
+    "Account",
+    "AccountDirector",
+    "Cluster",
+    "ClusterHead",
+    "FinancialRecord",
+    "LLMSettings",
+    "Project",
+    "ProjectManager",
+    "RefreshToken",
+    "Upload",
+    "User",
+    "UtilizationRecord",
+]
